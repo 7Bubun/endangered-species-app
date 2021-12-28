6176dc646306e79fc1f4dd3531064db5dddcf3ec
7Bubun <62255561+7Bubun@users.noreply.github.com>
2021-12-28 20:46:23 +0100

    Create unstyled navbar

--- react-web-app/src/components/Navbar.jsx
+++ react-web-app/src/components/Navbar.jsx
@@ -1,9 +1,10 @@
 import React, { Component } from "react";
+import "./Navbar.css";
 
 class Navbar extends Component {
     render() {
         return (
-            <div>
+            <div className="Navbar">
                 <button onClick={() => this.props.routingFunction("homeview")}>Home</button>
                 <button onClick={() => this.props.routingFunction("mapview")}>Map</button>
                 <button onClick={() => this.props.routingFunction("imageview")}>Image Classification</button>
