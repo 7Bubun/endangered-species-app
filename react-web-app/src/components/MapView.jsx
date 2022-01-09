f3fd348155c789036c90dac50d506aea456e748a
“Pawel <skok2022@gmail.com>
2022-01-09 21:09:37 +0100

    Added map

--- react-web-app/src/components/MapView.jsx
+++ react-web-app/src/components/MapView.jsx
@@ -1,8 +1,14 @@
 import React, { Component } from "react";
 
+import AzureMapCom from './App.js';
+
 class MapView extends Component {
     render() {
-        return <h4>mapka</h4>;
+        return (
+            <><h4>mapka</h4><div>
+                <AzureMapCom/>
+            </div></> 
+        );
     }
 }
 
