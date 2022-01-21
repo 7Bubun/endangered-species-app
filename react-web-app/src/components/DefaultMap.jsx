c71d7e44e1a3feb71b1506fb610f58972b27da58
“Pawel <skok2022@gmail.com>
2022-01-21 15:20:12 +0100

    Kinda working

--- react-web-app/src/components/DefaultMap.jsx
+++ react-web-app/src/components/DefaultMap.jsx
@@ -3,12 +3,19 @@ import {AzureMap, AzureMapsProvider, IAzureMapOptions} from 'react-azure-maps'
 import {AuthenticationType} from 'azure-maps-control'
 import Papa from 'papaparse'
 import { key } from "./key";
+
 const option = {
   authOptions: {
       authType: AuthenticationType.subscriptionKey,
       subscriptionKey: key
   },
 }
+
+Papa.parse("countries_codes_and_coordinates.csv", {
+  complete: function(results) {
+    console.log("Finished:", results.data);
+  }}
+)
 
 class DefaultMap extends Component {
   render() {
